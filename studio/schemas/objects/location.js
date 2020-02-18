0d30b0ee01416151e42f92166544cdaa31c09f71
mpazap <60862102+mpazap@users.noreply.github.com>
2020-02-18 06:25:23 -0800

    Create location.js

---
+++ studio/schemas/objects/location.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'location',
+  type: 'object',
+  title: 'Location',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name of   Location'
+    },
+    {
+      name: 'city',
+      type: 'string',
+      title: 'City'
+    },
+    {
+      name: 'locState',
+      type: 'string',
+      title: 'State'
+    }
+  ]
+}
